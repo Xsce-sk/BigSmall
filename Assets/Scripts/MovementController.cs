8854b51cc29960efa38cd77ec42fa3ede600265e
Christopher Jabs <cjabs97@gmail.com>
2019-04-18 16:55:09 -0700

    Add Movement Test Scene
    I also added a movement script to go along with the scene.

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [Header("Settings")]
+    public float moveSpeed;
+    public string verticalAxis;
+    public string horizontalAxis;
+
+    [Header("Debug")]
+    public bool debug;
+
+    [SerializeField] private Rigidbody2D m_RigidBody2D;
+
+    public void Awake()
+    {
+        m_RigidBody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        m_RigidBody2D.velocity = new Vector3(Input.GetAxis(horizontalAxis) * moveSpeed, Input.GetAxis(verticalAxis) * moveSpeed, 0f);
+    }
+}
