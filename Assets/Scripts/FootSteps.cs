bf4cb92bf8c18eefe70f3108d19cb42c29e3ef64
Christopher Jabs <cjabs97@gmail.com>
2019-04-23 11:20:03 -0700

    Add FootSteps
     footstep script and orthographic camera

---
+++ Assets/Scripts/FootSteps.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [Header("Settings")]
+    public float stepInterval;
+    public AudioClip stepSound;
+
+    [Header("Debug")]
+    [SerializeField] private AudioSource m_AudioSource;
+
+    private void Start()
+    {
+        m_AudioSource = this.GetComponent<AudioSource>();
+
+        StartCoroutine(StepLoop());
+    }
+
+    IEnumerator StepLoop()
+    {
+        while(true)
+        {
+            m_AudioSource.pitch = Random.Range(0.9f, 1.1f);
+            m_AudioSource.PlayOneShot(stepSound);
+            yield return new WaitForSeconds(stepInterval);
+        }
+    }
+}
