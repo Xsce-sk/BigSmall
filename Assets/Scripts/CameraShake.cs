723d8a8d439800a4f9837dd67ce213ba44bf9e22
Ace <ace.lowder@gmail.com>
2019-04-23 11:39:21 -0700

    Add Camera Shake
    Created Test_Footsteps scene

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    [Header("Debug")]
+    [SerializeField] bool m_IsShaking;
+    [SerializeField] Vector2 m_CurrentPos;
+    [SerializeField] Vector2 m_Offset;
+    [SerializeField] Transform m_Transform;
+
+    void Start()
+    {
+        m_Transform = transform;
+    }
+
+    public void Shake(float duration, float range = 0.1f, float step = 0.1f)
+    {
+        StartCoroutine(ShakeRoutine(duration, range, step));
+    }
+
+    IEnumerator ShakeRoutine(float duration, float range, float step)  
+    {
+        m_CurrentPos = m_Transform.position;
+
+        while (duration > 0)
+        {
+            m_Offset = new Vector2(Random.Range(-range / 2, range / 2), Random.Range(-range / 2, range / 2));
+            m_Transform.position = new Vector2(m_CurrentPos.x + m_Offset.x, m_CurrentPos.y + m_Offset.y);
+
+            yield return new WaitForEndOfFrame();
+            duration -= Time.deltaTime;
+
+            m_CurrentPos = new Vector2(m_Transform.position.x - m_Offset.x, m_Transform.position.y - m_Offset.y);
+        }
+
+        m_Transform.position = m_CurrentPos;
+        m_Offset = new Vector2();
+    }
+}
