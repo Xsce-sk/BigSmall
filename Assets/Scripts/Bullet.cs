79e72970059a8128bb423f49076622d46e6a8024
Christopher Jabs <cjabs97@gmail.com>
2019-04-23 12:27:58 -0700

    Add shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("Settings")]
+    public float bulletSpeed;
+    
+    [Header("Debug")]
+    [SerializeField] Transform m_Transform;
+    [SerializeField] Rigidbody2D m_Rigidbody2D;
+
+    void Start()
+    {
+        m_Transform = this.transform;
+        m_Rigidbody2D = this.GetComponent<Rigidbody2D>();
+        m_Rigidbody2D.AddForce(m_Transform.right * bulletSpeed, ForceMode2D.Impulse);
+    }
+
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class Shooter : MonoBehaviour
+{
+    //[Serializable]
+ 
+
+    [Header("Settings")]
+    public GameObject bulletPrefab;
+    public KeyCode shootKey;
+    public UnityEvent OnShoot;
+    public Vector2 offset;
+
+
+    [Header("Debug")]
+    [SerializeField] private Transform m_Transform;
+
+    void Start()
+    {
+        m_Transform = this.transform;
+    }
+
+ 
+    void Update()
+    {
+        if(Input.GetKeyDown(shootKey))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        OnShoot.Invoke();
+        Vector3 spawnOffset = (m_Transform.right * offset.x) +  (m_Transform.up * offset.y);
+
+        Vector3 spawnPos = m_Transform.position + spawnOffset;
+
+        Instantiate(bulletPrefab, spawnPos, m_Transform.rotation);
+    }
+}
