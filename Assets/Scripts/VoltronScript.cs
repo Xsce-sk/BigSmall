f342d9ffd2a7cd8581d963e27958f55ae3ab7f1a
Ace <ace.lowder@gmail.com>
2019-04-29 15:27:49 -0700

    Add Voltron Functionality

--- Assets/Scripts/VoltronScript.cs
+++ Assets/Scripts/VoltronScript.cs
@@ -4,63 +4,100 @@ using UnityEngine;
 
 public class VoltronScript : MonoBehaviour
 {
-    public Transform smallsTransform;
-    public Rigidbody2D smallsRB2D;
-    public Animator smallsAnimator;
-    public Transform biggumsTransform;
-    public MovementController smallsMC;
-    public float voltronDistance = 1f;
+    [Header("Settings")]
+    public float voltronDistance;
+    public float voltronDuration;
+    public float jumpoffDistance;
     public KeyCode voltronKey;
-    public Transform biggumBack;
-    
-    //public float cooldown;
 
-    [SerializeField] private bool onBack = false;
-    [SerializeField] private bool acceptInput = true;
+    [Header("Biggums")]
+    public Transform biggumsTransform;
+    public Transform biggumBack;
+
+    [Header("Debug")]
+    [SerializeField] private Transform m_Transform;
+    [SerializeField] private MovementController m_MovementController;
+    [SerializeField] private Rigidbody2D m_Rigidbody2D;
+    [SerializeField] private Animator m_Animator;
+    [SerializeField] private bool m_OnBack = false;
+    [SerializeField] private bool m_IsAcceptingInput = true;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        m_Transform = transform;
+        m_MovementController = GetComponent<MovementController>();
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (onBack && Input.GetKeyDown(voltronKey) && acceptInput)
+        if (WithinVoltronDistance() && Input.GetKeyDown(voltronKey) && !m_OnBack && m_IsAcceptingInput)
         {
-            Debug.Log("aaa");
-            smallsTransform.parent = null;
-            Debug.Log("b");
-            smallsMC.enabled = true;
-            Debug.Log("c");
-            onBack = false;
-            StartCoroutine(HitCooldown());
+            m_Animator.SetBool("IsJumping", true);
+            StartCoroutine(Combine());
+
+            m_MovementController.enabled = false;
+            m_OnBack = true;
+
+            m_Rigidbody2D.velocity = Vector2.zero;
+            m_Animator.SetFloat("Velocity", 0);
         }
 
-        if (WithinVoltronDistance() && Input.GetKeyDown(voltronKey) && !onBack && acceptInput)
+        if (m_OnBack && Input.GetKeyDown(voltronKey) && m_IsAcceptingInput)
         {
-            smallsTransform.parent = biggumsTransform;
-            smallsTransform.localPosition = biggumBack.localPosition;
-            smallsMC.enabled = false;
-            onBack = true;
-            smallsRB2D.velocity = Vector2.zero;
-            smallsAnimator.SetFloat("Velocity", 0);
-            StartCoroutine(HitCooldown());
+            m_Transform.parent = null;
+            
+            m_OnBack = false;
+
+            m_Animator.SetBool("IsJumping", true);
+            StartCoroutine(Disassemble());
         }
     }
 
-    IEnumerator HitCooldown()
+    IEnumerator Combine()
     {
-        acceptInput = !acceptInput;
-        yield return new WaitForSeconds(Time.deltaTime);
-        acceptInput = !acceptInput;
+        m_IsAcceptingInput = false;
+        float t = 0;
+        Vector3 startPos = m_Transform.position;
 
+        while (t < 1)
+        {
+            m_Transform.position = Vector3.Lerp(startPos, biggumBack.position, t);
+            yield return new WaitForEndOfFrame();
+            t += Time.deltaTime/voltronDuration;
+        }
+
+        m_Transform.parent = biggumsTransform;
+        m_Transform.localPosition = biggumBack.localPosition;
+
+        m_Animator.SetBool("IsJumping", false);
+        m_IsAcceptingInput = true;
+    }
+
+    IEnumerator Disassemble()
+    {
+        m_IsAcceptingInput = false;
+        float t = 0;
+        Vector3 startPos = m_Transform.position;
+        Vector3 targetPos = Vector3.MoveTowards(startPos, Camera.main.transform.position, jumpoffDistance);
+        while (t < 1)
+        {
+            m_Transform.position = Vector3.Lerp(startPos, targetPos, t);
+            yield return new WaitForEndOfFrame();
+            t += Time.deltaTime / voltronDuration;
+        }
+
+        m_MovementController.enabled = true;
+        m_Animator.SetBool("IsJumping", false);
+        m_IsAcceptingInput = true;
     }
 
     bool WithinVoltronDistance()
     {
-        if(Vector3.Distance(smallsTransform.position, biggumsTransform.position) <= voltronDistance)
+        if(Vector3.Distance(m_Transform.position, biggumsTransform.position) <= voltronDistance)
             return true;
 
         return false;
