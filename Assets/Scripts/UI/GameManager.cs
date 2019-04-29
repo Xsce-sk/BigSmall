8718b3575cc6d556489de5df12f717190e50bb99
Ace <ace.lowder@gmail.com>
2019-04-29 12:31:20 -0700

    Add Functionality to Main Menu

---
+++ Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void MoveToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MoveToScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
