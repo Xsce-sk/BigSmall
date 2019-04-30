c671a9633287e29576ee6d5c5dfc01a8751caf3d
Ace <ace.lowder@gmail.com>
2019-04-30 13:36:38 -0700

    Add Level Transition

---
+++ Assets/Scripts/UI/LevelManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("Settings")]
+    public bool lastLevel;
+
+    [Header("Debug")]
+    [SerializeField] int m_ShieldEnemyCount;
+    [SerializeField] int m_SpikeEnemyCount;
+    [SerializeField] int m_ArmorEnemyCount;
+    [SerializeField] bool m_IsChangingLevel;
+    [SerializeField] LevelTransition m_LevelTransition;
+
+    private void Start()
+    {
+        m_LevelTransition = GameObject.Find("LevelTransition").GetComponent<LevelTransition>();
+    }
+
+    void Update()
+    {
+        m_ShieldEnemyCount = GameObject.FindGameObjectsWithTag("ShieldEnemy").Length;
+        m_SpikeEnemyCount = GameObject.FindGameObjectsWithTag("SpikeEnemy").Length;
+        m_ArmorEnemyCount = GameObject.FindGameObjectsWithTag("ArmorEnemy").Length;
+        if (m_ShieldEnemyCount + m_SpikeEnemyCount + m_ArmorEnemyCount == 0)
+        {
+            if (lastLevel)
+            {
+                EndGame();
+            }
+            else
+            {
+                Win();
+            }
+        }
+    }
+
+    public void Win()
+    {
+        if (!m_IsChangingLevel)
+        {
+            m_LevelTransition.MoveToNextScene();
+            m_IsChangingLevel = true;
+        }
+    }
+
+    public void Lose()
+    {
+        // Add This
+    }
+
+    public void EndGame()
+    {
+        // Add This
+    }
+}
---
+++ Assets/Scripts/UI/LevelTransition.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    [Header("Debug")]
+    [SerializeField] Transform m_Transform;
+
+    void Start()
+    {
+        m_Transform = transform;
+
+        StartCoroutine(TransitionAnimation());
+    }
+
+    public void MoveToNextScene()
+    {
+        MoveToScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MoveToScene(int index)
+    {
+        StartCoroutine(ChangeScene(index));
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator ChangeScene(int index)
+    {
+        float t = 0;
+        Vector3 startPos = m_Transform.position;
+        Vector3 targetPos = new Vector3(0, 0, 0);
+        while (t < 1)
+        {
+            m_Transform.position = Vector3.Lerp(startPos, targetPos, t);
+            yield return new WaitForEndOfFrame();
+            t += Time.deltaTime;
+        }
+
+        SceneManager.LoadScene(index);
+        print("Loading Scene: " + index);
+    }
+
+    IEnumerator TransitionAnimation()
+    {
+        m_Transform.position = new Vector3(0, 0, 0);
+
+        float t = 0;
+        Vector3 startPos = m_Transform.position;
+        Vector3 targetPos = new Vector3(42, 0, 0);
+        while (t < 1)
+        {
+            m_Transform.position = Vector3.Lerp(startPos, targetPos, t);
+            yield return new WaitForEndOfFrame();
+            t += Time.deltaTime;
+        }
+
+        m_Transform.position = new Vector3(-42, 0, 0);
+    }
+}
